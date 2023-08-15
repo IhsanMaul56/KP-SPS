4838cda83cf5c109e56a015350542473827e1f69
IhsanMaul56 <115138037+IhsanMaul56@users.noreply.github.com>
2023-08-15 15:47:09 +0700

    Pembuatan Sidebar

--- resources/views/data_guru.blade.php
+++ resources/views/data_guru.blade.php
@@ -2,21 +2,7 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row">
-            <div class="custom-div">
-                <center>
-                    <img src="{{URL::asset('/img/logosmk1@2x.png')}}" class="img-fluid mt-4 mb-3" alt="logo smk" height="120" width="120">
-                    <br><h3 class="text-white">SMK SANGKURIANG <br> 1 CIMAHI</h3>
-                </center>
-                <div class="col">
-                    <center>
-                    <span class="btn btn-primary"><i class="fa-brands fa-bootstrap"></i>Beranda</span><br>
-                    <span class="btn btn-primary">Beranda</span><br>
-                    <span class="btn btn-primary">Beranda</span>
-                    </center>
-                </div>
-            </div>
-        
+        @include('partials.sidebar')
             <div class="col-md-4">
                 <div class="custom-div2">
                     
@@ -31,29 +17,4 @@
 
         </div>
     </div>
-
 @endsection
-{{-- @extends('layouts.app')
-
-@section('content')
-<div class="main-continer d-flex">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="custom-div">
-                
-            </div>
-        </div>
-    </div>
-    <nav class="sidebar" id="side_nav">
-        <div class="header-box">
-            <h1 class="fs-4">
-                <span>Selamat datang GFU</span>
-            </h1>
-        </div>
-    </nav>
-    <div class="content">
-        <h1>{{ Auth::user()->name }}</h1>
-        <a href="/logout">logout</a>
-    </div>
-</div>
-@endsection --}}
