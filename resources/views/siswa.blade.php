bd644bbf75fefbe6759ffdb25918640dee92cba1
ZOEXYZ\lil Uzoyy <afuzadwi69@gmail.com>
2023-08-20 22:48:24 +0700

    fixx issues 2

---
+++ resources/views/siswa.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    @include('partials.sidebar')
+    <div class="row">
+        <div class="col-md-4">
+            <div class="grid-tengah">
+                <h1 class="font">Beranda</h1>
+            </div>
+            <div class="card" style="position: absolute; z-index: 2;  width: 696px; height: 449px; bottom: 191px; top: 100px">
+                <div class="card-body">
+                    <div class="row" style="position: right">
+                        <span>Semester Ganjil</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="grid-kanan">
+            <div class="">
+                
+            </div>
+        </div>
+
+    </div>
+    </div>
+
+@endsection
