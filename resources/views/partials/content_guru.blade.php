b37dddd5b53ea7168d816193edf2f44a4f703367
ZOEXYZ\lil Uzoyy <afuzadwi69@gmail.com>
2023-08-21 18:15:18 +0700

    penambahan js dan template content serta rightbar

---
+++ resources/views/partials/content_guru.blade.php
@@ -0,0 +1,95 @@
+{{-- Beranda --}}
+<div class="col p-0 page active-page" id="page1">
+    <div class="grid-tengah">
+        <div class="row">
+            <div class="col">
+                <span class="h1 fw-bold text-biru">Beranda</span>
+            </div>
+            <div class="col text-end">
+                <span class="h5">Selamat Datang,</span><br>
+                <span class="h4 fw-bold">{{ Auth::user()->name }}</span>
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="card-body" id="shadow">
+                <div class="row">
+                    <div class="col">
+                        <div class="persegi">
+                            <p class="text-white">Semester Guru</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="persegi2">
+                            <p class="text-white">X RPL 1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @include('partials.footer')
+        </div>
+    </div>
+</div>
+
+{{-- Siswa --}}
+<div class="col p-0 page" id="page2">
+    <div class="grid-tengah">
+        <div class="row">
+            <div class="col">
+                <span class="h1 fw-bold text-biru">Ukan</span>
+            </div>
+            <div class="col text-end">
+                <span class="h5">Selamat Datang,</span><br>
+                <span class="h4 fw-bold">{{ Auth::user()->name }}</span>
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="card-body" id="shadow">
+                <div class="row">
+                    <div class="col">
+                        <div class="persegi">
+                            <p class="text-white">Semester Guru</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="persegi2">
+                            <p class="text-white">X RPL 1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @include('partials.footer')
+        </div>
+    </div>
+</div>
+
+{{-- Akun --}}
+<div class="col p-0 page" id="page3">
+    <div class="grid-tengah">
+        <div class="row">
+            <div class="col">
+                <span class="h1 fw-bold text-biru">AKun</span>
+            </div>
+            <div class="col text-end">
+                <span class="h5">Selamat Datang,</span><br>
+                <span class="h4 fw-bold">{{ Auth::user()->name }}</span>
+            </div>
+        </div>
+        <div class="row p-0 m-0">
+            <div class="card-body" id="shadow">
+                <div class="row">
+                    <div class="col">
+                        <div class="persegi">
+                            <p class="text-white">Semester Guru</p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="persegi2">
+                            <p class="text-white">X RPL 1</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @include('partials.footer')
+        </div>
+    </div>
+</div>
