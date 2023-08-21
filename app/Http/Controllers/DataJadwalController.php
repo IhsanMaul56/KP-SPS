b59b4627f3b963505322898ab1d03469bb907718
IhsanMaul56 <115138037+IhsanMaul56@users.noreply.github.com>
2023-08-21 13:21:58 +0700

    View data guru

---
+++ app/Http/Controllers/DataJadwalController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\data_jadwal;
+use App\Http\Requests\Storedata_jadwalRequest;
+use App\Http\Requests\Updatedata_jadwalRequest;
+
+class DataJadwalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Storedata_jadwalRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(data_jadwal $data_jadwal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(data_jadwal $data_jadwal)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Updatedata_jadwalRequest $request, data_jadwal $data_jadwal)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(data_jadwal $data_jadwal)
+    {
+        //
+    }
+}
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -75,8 +75,10 @@
                                 <i class="bi bi-gear"></i>
                             </div>
                             <div class="col">
-                                <span><strong>NIS</strong></span><br>
-                                <span>{{ Auth::user()->name }}</></span>
+                                @foreach ($dataGuru as $data)
+                                    <span><strong>{{ $data->nip }}</strong></span><br>
+                                    <span>{{ $data->nama_guru }}</span>
+                                @endforeach
                                 <div class="card-kanan" id="shadow">
                                     <div class="card-body">
                                         <p>Pengumuman</p>
@@ -86,5 +88,6 @@
                         </div>
                     </div>
                 </div>
+            </div>    
         </div>
     @endsection
