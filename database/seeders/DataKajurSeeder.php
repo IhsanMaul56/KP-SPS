506d40e3ee2c2ea9c4bc83f9399e9ab2dc7a63c7
IhsanMaul56 <115138037+IhsanMaul56@users.noreply.github.com>
2023-08-26 16:32:19 +0700

    Data Seeder

--- database/seeders/DataKajurSeeder.php
+++ database/seeders/DataKajurSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\data_kajur;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,27 @@ class DataKajurSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $dataKajur = [
+            [
+                'kajur_id' => '1000',
+                'nama_guru' => 'Nasrullah Nurul R, S.Pd., M.Pd.'
+            ],
+            [
+                'kajur_id' => '2000',
+                'nama_guru' => 'Dr. Samsori, S.Pd., M.Pd'
+            ],
+            [
+                'kajur_id' => '3000',
+                'nama_guru' => 'Lukman S.Pd'
+            ],
+            [
+                'kajur_id' => '4000',
+                'nama_guru' => 'Farhan S.Pd'
+            ]
+        ];
+
+        foreach ($dataKajur as $key => $val){
+            data_kajur::create($val);
+        }
     }
 }
--- database/seeders/DataMapelSeeder.php
+++ database/seeders/DataMapelSeeder.php
@@ -15,44 +15,31 @@ class DataMapelSeeder extends Seeder
     {
         $dataMapel = [
             [
-                'nama_mapel' => 'Akuntansi Keuangan',
-                'nip' => '0001',
+                'nama_mapel' => 'Akuntansi Keuangan'
             ],
             [
                 'nama_mapel' => 'Bahasa Inggris',
-                'nip' => '0002',
             ],
             [
                 'nama_mapel' => 'Pendidikan Pancasila dan Kewarganegaraan',
-                'nip' => '0003',
             ],
             [
                 'nama_mapel' => 'Praktikum Akuntansi Perusahaan Jasa/Dagang/Manufaktur',
-                'nip' => '0004',
             ],
             [
                 'nama_mapel' => 'Pendidikan Agama dan Budi Pekerti',
-                'nip' => '0005',
             ],
             [
                 'nama_mapel' => 'Otomatisasi Tata Kelola Humas dan Keprotokolan',
-                'nip' => '0006',
             ],
             [
                 'nama_mapel' => 'Produk Kreatif dan Kewirausahaan',
-                'nip' => '0007',
             ],
             [
                 'nama_mapel' => 'Bahasa Indonesia',
-                'nip' => '0008',
-            ],
-            [
-                'nama_mapel' => 'Bahasa Inggris',
-                'nip' => '0009',
             ],
             [
                 'nama_mapel' => 'Praktikum Akuntansi Lembaga/Instansi Pemerintah',
-                'nip' => '0010',
             ],
         ];
 
