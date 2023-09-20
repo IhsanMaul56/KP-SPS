a23ed56e5e46cf434675fbd537b530d4fe2492ca
IhsanMaul56 <115138037+IhsanMaul56@users.noreply.github.com>
2023-09-20 12:35:05 +0700

    Menambah UI master jadwal

---
+++ app/Http/Livewire/DataJadwal.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\data_jadwal;
+use App\Models\data_pengampu;
+use Livewire\WithPagination;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class DataJadwal extends Component
+{
+    public $jadwal;
+    public $pengampu;
+
+    public $search = '';
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    public function render()
+    {
+        $user = Auth::user();
+
+        if ($user && $user->admin_id) {
+            $this->jadwal = DB::table('data_jadwals')
+                ->select('data_jadwals.*')
+                ->get();
+
+            if ($this->jadwal) {
+                $pengampuIds = $this->jadwal->pluck('pengampu_id')->toArray();
+
+                // Fetch all matching data_pengampus records using whereIn
+                $this->pengampu = DB::table('data_pengampus')
+                    ->whereIn('kode_pengampu', $pengampuIds)
+                    ->get();
+            } else {
+                $this->pengampu = null;
+            }
+        } else {
+            $this->pengampu = null;
+            $this->jadwal = null;
+        }
+
+        return view('livewire.data-jadwal', [
+            'dataJadwal' => data_jadwal::where('hari', 'like', '%'.$this->search.'%')->paginate(10)
+        ]);
+    }
+
+    public function updatingSearch(){
+        $this->resetPage();
+    }
+}
--- resources/views/partials/content_admin.blade.php
+++ resources/views/partials/content_admin.blade.php
@@ -117,7 +117,7 @@
     <div class="grid-tengah">
         <div class="row">
             <div class="col">
-                <span class="h1 fw-bold text-biru">Jadwal Guru</span>
+                <span class="h1 fw-bold text-biru">AKADEMIK | Jadwal Pelajaran</span>
             </div>
             <div class="col text-end">
                 <span class="h5">Selamat Datang,</span><br>
@@ -125,20 +125,7 @@
             </div>
         </div>
         <div class="row p-0 m-0">
-            <div class="card-body h-100 overflow-auto" id="shadow">
-                <div class="row">
-                    <div class="col">
-                        <div class="persegi">
-                            <p class="text-white">Semester Admin</p>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="persegi2">
-                            <p class="text-white">X RPL 1</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @livewire('data-jadwal')
         </div>
     </div>
 </div>
