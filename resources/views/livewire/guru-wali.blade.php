5f7c0ade4b3633950286da47c76bcf63327f28e6
IhsanMaul56 <115138037+IhsanMaul56@users.noreply.github.com>
2023-10-16 21:15:57 +0700

    Crud data guru

--- resources/views/livewire/guru-wali.blade.php
+++ resources/views/livewire/guru-wali.blade.php
@@ -14,9 +14,11 @@
                         </div>
                     </div>
                 </div>
-            </div>
-            <input type="text" class="form-control" wire:model="search" placeholder="Cari" style="border-color: rgba(168, 168, 168, 1); width: 250px; border-radius: 10px 10px 10px 10px;">
-            <table class="table">
+        </div>
+        <div class="row">
+            <input type="text" class="form-control" wire:model="search" placeholder="Cari"
+                style="border-color: rgba(168, 168, 168, 1); width: 250px; border-radius: 10px 10px 10px 10px;">
+            <table class="table table-bordered m-3">
                 <thead>
                     <tr class="text-center">
                         <th>No</th>
@@ -32,27 +34,29 @@
                     <?php $no = 1; ?>
                     @foreach ($dataSiswa as $item)
                         <tr class="text-center">
-                            <th>No</th>
-                            <th>NIS</th>
-                            <th>Nama Siswa</th>
-                            <th>Jenis Kelamin</th>
-                            <th>Nilai</th>
-                            <th>Keterangan</th>
+                            <th>{{ $no++ }}</th>
+                            <th>{{ $item->nis }}</th>
+                            <th>{{ $item->nama_siswa }}</th>
+                            <th>{{ $item->jenis_kelamin }}</th>
+                            <th>0</th>
+                            <th>Baba</th>
                             <th>Aksi</th>
                         </tr>
                     @endforeach
                 </tbody>
             </table>
             <div class="col text-end mt-3">
-                <button class="btn btn-success" id="shadow" type="submit" style="position: relative;">Setujui</button>
+                <button class="btn btn-success" id="shadow" type="submit"
+                    style="position: relative;">Setujui</button>
             </div>
+        </div>
         @else
-            <div class="col text-center">
-                <img src="{{URL::asset('/img/warning.png')}}" alt="warning" width="125px;">
-            </div>
-            <div class="col text-center pt-3">
-                <span class="mt-3">Halaman Belum Bisa Diakses</span>
-            </div>
+        <div class="col text-center">
+            <img src="{{ URL::asset('/img/warning.png') }}" alt="warning" width="125px;">
+        </div>
+        <div class="col text-center pt-3">
+            <span class="mt-3">Halaman Belum Bisa Diakses</span>
+        </div>
         @endif
     </div>
 </div>
